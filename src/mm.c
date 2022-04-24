31e32d041f34f70ff2149d7fecf5350eaf4b3dd8
arlyviac <chaialee@usc.edu>
2022-04-24 16:18:30 -0700

    about 90/100

--- src/mm.c
+++ src/mm.c
@@ -25,23 +25,37 @@ static BlockHeader *free_coalesce(BlockHeader *bp) {
     int prev_alloc = mm_block_allocated(mm_block_prev(bp));
     int next_alloc = mm_block_allocated(mm_block_next(bp));
 
-    if (prev_alloc && next_alloc) {
+    if (prev_alloc && next_alloc) { //alloc-alloc
         // TODO: add bp to free list
+        mm_list_append(bp);
         return bp;
-
-    } else if (prev_alloc && !next_alloc) {
+    } else if (prev_alloc && !next_alloc) { //alloc-free
         // TODO: remove next block from free list
+        mm_list_remove(mm_block_next(bp));
         // TODO: add bp to free list
         // TODO: coalesce with next block
+        size += mm_block_size(mm_block_next(bp));
+        mm_block_set_header(bp, size, 0);
+        mm_block_set_footer(bp, size, 0);
+        mm_list_append(bp);
         return bp;
-
-    } else if (!prev_alloc && next_alloc) {
+    } else if (!prev_alloc && next_alloc) { //free-alloc
         // TODO: coalesce with previous block
+        size += mm_block_size(mm_block_prev(bp));
+        mm_block_set_header(mm_block_prev(bp), size, 0);
+        mm_block_set_footer(mm_block_prev(bp), size, 0);
         return mm_block_prev(bp);
 
-    } else {
+    } else { //free-free
+        BlockHeader *prev_free = mm_block_prev(bp);
+        BlockHeader *next_free = mm_block_next(bp);
         // TODO: remove next block from free list
+        mm_list_remove(next_free);
         // TODO: coalesce with previous and next block
+        size += mm_block_size(prev_free);
+        size += mm_block_size(next_free);
+        mm_block_set_header(prev_free, size, 0);
+        mm_block_set_footer(prev_free, size, 0);
         return mm_block_prev(bp);
     }
 }
@@ -89,12 +103,17 @@ int mm_init(void) {
     heap_blocks += 1;                            // point to the prologue header
 
     // TODO: extend heap with an initial heap size
-
+    if (extend_heap(5000) == NULL) return -1;
     return 0;
 }
 
 void mm_free(void *bp) {
     // TODO: move back 4 bytes to find the block header, then free block
+    if(bp == 0) return;
+
+    bp = bp - 4;
+
+    free_coalesce(bp);
 }
 
 /**
@@ -106,6 +125,25 @@ void mm_free(void *bp) {
  */
 static BlockHeader *find_fit(int size) {
     // TODO: implement
+    //if item is less than some threshold, put at front of free space
+    //if larger, put at back
+    if(size > 1024){
+        BlockHeader *tmp = mm_list_headp;
+        while(tmp != 0){
+            if (mm_block_size(tmp) >= size){
+                return tmp;
+            }
+            tmp = mm_list_next(tmp);
+        }
+    } else {
+        BlockHeader *tmp = mm_list_tailp;
+        while(tmp != 0){
+            if (mm_block_size(tmp) >= size){
+                return tmp;
+            }
+            tmp = mm_list_prev(tmp);
+        }
+    }
     return NULL;
 }
 
@@ -119,7 +157,40 @@ static BlockHeader *find_fit(int size) {
 static BlockHeader *place(BlockHeader *bp, int size) {
     // TODO: if current size is greater, use part and add rest to free list
     // TODO: return pointer to header of allocated block
-    return NULL;
+    int bp_size = mm_block_size(bp);
+
+    int rest = bp_size - size;
+
+    mm_list_remove(bp);
+
+    //threshold, 1024 or 512
+    if(rest > 512){
+        if(rest > 1024){
+            mm_block_set_header(bp, rest, 0);
+            mm_block_set_footer(bp, rest, 0);
+
+            mm_list_append(bp);
+
+            mm_block_set_header(mm_block_next(bp), size, 1);
+            mm_block_set_footer(mm_block_next(bp), size, 1);
+
+            return mm_block_next(bp);
+        } else {
+            mm_block_set_header(bp, size, 1);
+            mm_block_set_footer(bp, size, 1);
+
+            mm_block_set_header(mm_block_next(bp), rest, 0);
+            mm_block_set_footer(mm_block_next(bp), rest, 0);
+
+            free_coalesce(mm_block_next(bp));
+
+            return bp;
+        }
+    } else {
+        mm_block_set_header(bp, bp_size, 1);
+        mm_block_set_footer(bp, bp_size, 1);
+    }
+    return bp;
 }
 
 /**
@@ -139,10 +210,21 @@ void *mm_malloc(size_t size) {
     if (size == 0)
         return NULL;
 
-    int required_size = required_block_size(size);
 
+    int required_size = required_block_size(size);
+    
     // TODO: find a free block or extend heap
     // TODO: allocate and return pointer to payload
+    int block_extension = MAX(4096, required_size);
+    BlockHeader *tmp = find_fit(required_size);
+
+    if(tmp != NULL){
+        return place(tmp, required_size) + 1;
+    } else {
+        tmp = extend_heap(block_extension);
+        return place(tmp, required_size) + 1;
+    }
+
     return NULL;
 }
 
@@ -163,9 +245,39 @@ void *mm_realloc(void *ptr, size_t size) {
         // TODO: return pointer to payload of new block
 
         // TODO: remove this naive implementation
-        void *new_ptr = mm_malloc(size);
+        /*void *new_ptr = mm_malloc(size);
         memcpy(new_ptr, ptr, MIN(size, (unsigned)mm_block_size(ptr-4) - 8));
         mm_free(ptr);
-        return new_ptr;
+        return new_ptr;*/
+
+        BlockHeader *ptr_block = (BlockHeader *)ptr-1;
+        BlockHeader *next_block = mm_block_next((BlockHeader *)ptr-1);
+
+        int ptr_block_size = mm_block_size((BlockHeader *)ptr-1);
+        int next_size = mm_block_size(next_block);
+        int required_size = required_block_size(size);
+        int new_size = ptr_block_size + next_size;
+
+        if(ptr_block_size >= required_size){
+            return ptr;
+        }
+
+        if((next_block != 0) && (new_size >= required_size) && (!mm_block_allocated(next_block))){
+            mm_list_remove(next_block);
+
+            mm_block_set_header(ptr_block, new_size, 1);
+            mm_block_set_footer(ptr_block, new_size, 1);
+
+            return ptr;
+        }
+
+        //for copy
+        int num_bytes = required_size - 8;
+
+        ptr_block = mm_malloc(required_size);
+        memcpy(ptr_block, ptr, num_bytes);
+        mm_free(ptr);
+
+        return ptr_block;
     }
 }
